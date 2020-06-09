7039732a9d770f633a0d52b98e03648476938bc3
DoQuangThanh <thanhdoquang@live.com>
2020-06-09 09:50:38 +0700

    first commit

---
+++ ProLinks.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Pro Links
+Plugin URI: https://aitech.asia/wordpress/#prolinks
+Description: Short Link, track and share any URL using your website and brand!
+Version: .1.1
+Author:  Aitech Asia
+Author URI: https://aitech.asia
+Text Domain: pro-link
+Copyright: 2020 aitech Asia
+*/
